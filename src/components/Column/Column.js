8c36447f76d9d8ed2a8d7be707685818aad7b824
mjalowiczor <m.jalowiczor@hotmail.com>
2019-11-13 18:29:57 +0100

    add Icon funcion

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,11 +3,13 @@ import styles from "./Column.scss";
 import PropTypes from "prop-types";
 import Card from "../Card/Card.js";
 import Creator from "../Creator/Creator.js";
+import Icon from "../Button/Icon.js";
 import { settings } from "../../data/dataStore";
 
 class Column extends React.Component {
   state = {
-    cards: this.props.cards || []
+    cards: this.props.cards || [],
+    icon: this.props.icon || []
   };
   static propTypes = {
     titleText: PropTypes.string
@@ -26,10 +28,15 @@ class Column extends React.Component {
     }));
   }
   render() {
-    // console.log("this", this.props.cards);
+    console.log("thisColumn", this.props.icon);
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}</h3>
+        <h3 className={styles.title}>
+          <span className={styles.icon}>
+            <Icon name={this.props.icon}></Icon>
+          </span>
+          {this.props.title}
+        </h3>
         {this.state.cards.map(({ key, ...cardsProps }) => (
           <Card key={key} {...cardsProps} />
         ))}
